5bdae69da6bba4eed4632f32b8baea796287eafb
jianliang wang <83890025+wang-jianliang@users.noreply.github.com>
2024-02-19 05:16:20 +0000

    fix: avoid access storage

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import '@pages/popup/Popup.css';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
-
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
+    <div className="App">
       <img className="icon" src="../../../icon-full.png" alt="Power Tabs" />
     </div>
   );
